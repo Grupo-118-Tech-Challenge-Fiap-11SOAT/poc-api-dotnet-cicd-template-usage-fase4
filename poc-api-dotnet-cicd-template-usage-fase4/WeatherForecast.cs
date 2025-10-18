9a6b3bdb7d6ac34079ee9693d04c678a9b44d421
tuliorezende <tulio.rezende@outlook.com>
2025-10-18 09:14:30 -0300

    feat: add initial ASP.NET Core project with Docker support

---
+++ poc-api-dotnet-cicd-template-usage-fase4/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace poc_api_dotnet_cicd_template_usage_fase4;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
